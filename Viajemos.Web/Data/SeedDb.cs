55c0f460031c1d093f8ce59f0ad7d2499d52957f
Tito <tito.parra4@@hotmail.com>
2021-02-13 21:17:27 -0500

    Add Seed DB

---
+++ Viajemos.Web/Data/SeedDb.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Viajemos.Web.Data.Entities;
+
+namespace Viajemos.Web.Data
+{
+    public class SeedDb
+    {
+        private readonly DataContext _dataContext;
+
+        public SeedDb(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        public async Task SeedAsync()
+        {
+            await _dataContext.Database.EnsureCreatedAsync();
+            await CheckEditorialsAsync();
+            await CheckAutorsAsync();
+            await CheckLibrosAsync();
+        }
+
+        private async Task CheckEditorialsAsync()
+        {
+            if (!_dataContext.Editorials.Any())
+            {
+                _dataContext.Editorials.Add(new Entities.Editorial { Name = "Nombre", Sede = "Bogota" });
+                await _dataContext.SaveChangesAsync();
+            }
+        }
+        private async Task CheckLibrosAsync()
+        {
+            var autor = _dataContext.Autors.FirstOrDefault();
+            var editorial = _dataContext.Editorials.FirstOrDefault();
+            if (!_dataContext.Libros.Any())
+            {
+                AddLibro("Net Core", "Es un sucesor multiplataforma de .NET Framework.", autor, editorial, 100);
+                await _dataContext.SaveChangesAsync();
+            }
+        }
+
+        private void AddLibro(
+                                string titulo,
+                                string sinopsis,
+                                Autor autor,
+                                Editorial editorial,
+                                int paginas)
+            {
+                _dataContext.Libros.Add(new Libro
+                {
+                    Titulo = titulo,
+                    Sinopsis = sinopsis,
+                    Autor = autor,
+                    Editorial = editorial,
+                    Paginas = paginas
+                });
+        }
+
+        private async Task CheckAutorsAsync()
+        {
+            if (!_dataContext.Autors.Any())
+            {
+                AddAutor("Tito", "Parra");
+                await _dataContext.SaveChangesAsync();
+            }
+        }
+
+        private void AddAutor(
+            string nombre,
+            string apellido)
+        {
+            _dataContext.Autors.Add(new Autor
+            {
+                Nombre = nombre,
+                Apellido = apellido,
+            });
+        }
+    }
+
+}
--- Viajemos.Web/Program.cs
+++ Viajemos.Web/Program.cs
@@ -1,12 +1,8 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Viajemos.Web.Data;
+
 
 namespace Viajemos.Web
 {
@@ -14,11 +10,25 @@ namespace Viajemos.Web
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+            RunSeeding(host);
+            host.Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+        private static void RunSeeding(IWebHost host)
+        {
+            var scopeFactory = host.Services.GetService<IServiceScopeFactory>();
+            using (var scope = scopeFactory.CreateScope())
+            {
+                var seeder = scope.ServiceProvider.GetService<SeedDb>();
+                seeder.SeedAsync().Wait();
+            }
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args).UseStartup<Startup>();
+        }
     }
+
 }
